19bc6ffc0b02e0ec5cebca8fe556054c46bd954f
Stiven Henao <142904664+StivenHenao@users.noreply.github.com>
2025-04-25 16:25:55 -0500

    fix: linting python

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -18,15 +17,15 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
-
+        ]
+    )
 
     def __str__(self):
         return self.name
